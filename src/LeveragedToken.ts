568505f5d9b3eb99fb663681e52add403c5f087b
Chase Manning <chase@manning.dev>
2025-11-13 11:24:17 +0200

    :art: change to storing events as trades

--- src/LeveragedToken.ts
+++ src/LeveragedToken.ts
@@ -2,15 +2,49 @@ import { ponder } from "ponder:registry";
 import schema from "ponder:schema";
 import crypto from "crypto";
 
+// event Mint(address indexed minter, address indexed to, uint256 baseAmount, uint256 ltAmount);
 ponder.on("LeveragedToken:Mint", async ({ event, context }) => {
   const { minter, to, baseAmount, ltAmount } = event.args;
 
-  await context.db.insert(schema.leveragedTokenMint).values({
+  await context.db.insert(schema.trades).values({
     id: crypto.randomUUID(),
+    isBuy: true,
     timestamp: event.block.timestamp,
     leveragedToken: event.log.address,
     sender: minter,
     recipient: to,
+    baseAssetAmount: baseAmount,
+    leveragedTokenAmount: ltAmount,
+  });
+});
+
+// event Redeem(address indexed sender, address indexed to, uint256 ltAmount, uint256 baseAmount);
+ponder.on("LeveragedToken:Redeem", async ({ event, context }) => {
+  const { sender, to, ltAmount, baseAmount } = event.args;
+
+  await context.db.insert(schema.trades).values({
+    id: crypto.randomUUID(),
+    isBuy: false,
+    timestamp: event.block.timestamp,
+    leveragedToken: event.log.address,
+    sender: sender,
+    recipient: to,
+    baseAssetAmount: baseAmount,
+    leveragedTokenAmount: ltAmount,
+  });
+});
+
+// event ExecuteRedeem(address indexed user, uint256 ltAmount, uint256 baseAmount);
+ponder.on("LeveragedToken:ExecuteRedeem", async ({ event, context }) => {
+  const { user, ltAmount, baseAmount } = event.args;
+
+  await context.db.insert(schema.trades).values({
+    id: crypto.randomUUID(),
+    isBuy: false,
+    timestamp: event.block.timestamp,
+    leveragedToken: event.log.address,
+    sender: user,
+    recipient: user,
     baseAssetAmount: baseAmount,
     leveragedTokenAmount: ltAmount,
   });
