4c4dd8682f553461c864a66a1ebeeb0daa0a9273
Chase Manning <chase@manning.dev>
2025-12-01 14:56:57 +0000

    :wrench: trying with no start block

--- ponder.config.ts
+++ ponder.config.ts
@@ -9,9 +9,6 @@ export const LT_HELPER_ADDRESS = "0x560149730F1cb1594F15cF2186b4A86eC761c64D";
 export const FACTORY_ADDRESS = "0xaBD5D943b4Bb1D25C6639dD264243b246CC3aA51";
 export const REFERRALS_ADDRESS = "0x82A4063f4d05bb7BF18DF314DC5B63b655E86cBD";
 
-const factoryDeployed = 16730183;
-const ltsDeployed = 16731647;
-
 export default createConfig({
   chains: {
     hyperEvm: {
@@ -29,21 +26,17 @@ export default createConfig({
           "event CreateLeveragedToken(address indexed creator, address indexed token, uint32 indexed marketId, uint256 targetLeverage, bool isLong)"
         ),
         parameter: "token",
-        startBlock: factoryDeployed,
       }),
-      startBlock: ltsDeployed,
     },
     Factory: {
       chain: "hyperEvm",
       abi: FactoryAbi,
       address: FACTORY_ADDRESS,
-      startBlock: factoryDeployed,
     },
     Referrals: {
       chain: "hyperEvm",
       abi: ReferralsAbi,
       address: REFERRALS_ADDRESS,
-      startBlock: ltsDeployed,
     },
   },
 });
