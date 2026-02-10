b7b08e33e3f016fe09439f37a968e257c82498c7
Chase Manning <chase@manning.dev>
2026-02-10 16:32:32 +0000

    :art: improve typing

--- src/api/endpoints/get-trade.ts
+++ src/api/endpoints/get-trade.ts
@@ -1,9 +1,9 @@
 import { db } from "ponder:api";
 import schema from "ponder:schema";
 import { eq } from "drizzle-orm";
-import { Address } from "viem";
+import { Hex } from "viem";
 
-const getTrade = async (txHash: Address) => {
+const getTrade = async (txHash: Hex) => {
   try {
     const trade = await db.select().from(schema.trade).where(eq(schema.trade.originTxHash, txHash)).limit(1);
     if (!trade) return null;
