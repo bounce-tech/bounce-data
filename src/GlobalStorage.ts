0da65450c56c50a8e8ea6a25ffda2cd78511bfea
Chase Manning <chase@manning.dev>
2026-02-17 15:41:22 +0000

    :art: switch to onConflictDoUpdate for global storage indexing

--- src/GlobalStorage.ts
+++ src/GlobalStorage.ts
@@ -2,67 +2,82 @@ import { ponder } from "ponder:registry";
 import schema from "ponder:schema";
 import { GLOBAL_STORAGE_ID } from "./constants";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-async function ensureGlobalStorageRow(db: any) {
-  await db.insert(schema.globalStorage).values({ id: GLOBAL_STORAGE_ID }).onConflictDoNothing();
-}
-
 ponder.on("GlobalStorage:OwnershipTransferred", async ({ event, context }) => {
   const { newOwner } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ owner: newOwner });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, owner: newOwner })
+    .onConflictDoUpdate(() => ({ owner: newOwner }));
 });
 
 ponder.on("GlobalStorage:SetAllMintsPaused", async ({ event, context }) => {
   const { newPaused } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ allMintsPaused: newPaused });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, allMintsPaused: newPaused })
+    .onConflictDoUpdate(() => ({ allMintsPaused: newPaused }));
 });
 
 ponder.on("GlobalStorage:SetMinTransactionSize", async ({ event, context }) => {
   const { newMinTransactionSize } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ minTransactionSize: newMinTransactionSize });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, minTransactionSize: newMinTransactionSize })
+    .onConflictDoUpdate(() => ({ minTransactionSize: newMinTransactionSize }));
 });
 
 ponder.on("GlobalStorage:SetMinLockAmount", async ({ event, context }) => {
   const { newMinLockAmount } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ minLockAmount: newMinLockAmount });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, minLockAmount: newMinLockAmount })
+    .onConflictDoUpdate(() => ({ minLockAmount: newMinLockAmount }));
 });
 
 ponder.on("GlobalStorage:SetRedemptionFee", async ({ event, context }) => {
   const { newFee } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ redemptionFee: newFee });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, redemptionFee: newFee })
+    .onConflictDoUpdate(() => ({ redemptionFee: newFee }));
 });
 
 ponder.on("GlobalStorage:SetExecuteRedemptionFee", async ({ event, context }) => {
   const { newFee } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ executeRedemptionFee: newFee });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, executeRedemptionFee: newFee })
+    .onConflictDoUpdate(() => ({ executeRedemptionFee: newFee }));
 });
 
 ponder.on("GlobalStorage:SetStreamingFee", async ({ event, context }) => {
   const { newFee } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ streamingFee: newFee });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, streamingFee: newFee })
+    .onConflictDoUpdate(() => ({ streamingFee: newFee }));
 });
 
 ponder.on("GlobalStorage:SetTreasuryFeeShare", async ({ event, context }) => {
   const { newFeeShare } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ treasuryFeeShare: newFeeShare });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, treasuryFeeShare: newFeeShare })
+    .onConflictDoUpdate(() => ({ treasuryFeeShare: newFeeShare }));
 });
 
 ponder.on("GlobalStorage:SetReferrerRebate", async ({ event, context }) => {
   const { newRebate } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ referrerRebate: newRebate });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, referrerRebate: newRebate })
+    .onConflictDoUpdate(() => ({ referrerRebate: newRebate }));
 });
 
 ponder.on("GlobalStorage:SetRefereeRebate", async ({ event, context }) => {
   const { newRebate } = event.args;
-  await ensureGlobalStorageRow(context.db);
-  await context.db.update(schema.globalStorage, { id: GLOBAL_STORAGE_ID }).set({ refereeRebate: newRebate });
+  await context.db
+    .insert(schema.globalStorage)
+    .values({ id: GLOBAL_STORAGE_ID, refereeRebate: newRebate })
+    .onConflictDoUpdate(() => ({ refereeRebate: newRebate }));
 });
